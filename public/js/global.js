c5d74a267348ac380eefbb2ece81dc943965d830
SeanByrne04 <seanbyrne04@outlook.com>
2026-03-22 17:26:32 +0000

    Removed .env from tracking, moved add-photos.js to /pages folder

--- public/js/global.js
+++ public/js/global.js
@@ -58,7 +58,6 @@ loadComponent('footer', '/components/footer.html').then(() => {
     { path: 'friends', index: 2 },
     { path: 'chat', index: 3 },
     { path: 'me', index: 4 },
-    { path: 'help', index: 5 },
   ];
 
   navItems.forEach(item => item.classList.remove('active'));
